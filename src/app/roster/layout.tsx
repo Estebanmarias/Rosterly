064b08dfcc7ddff87476072cef2b773db13893c5
Estebanmarias <stephenbabatunde98@gmail.com>
2026-04-08 19:24:32 +0100

    feat: add roster layout component

---
+++ src/app/roster/layout.tsx
@@ -0,0 +1,6 @@
+// src/app/roster/layout.tsx
+export const dynamic = "force-dynamic";
+
+export default function RosterLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
